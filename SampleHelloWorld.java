f98ce3442033c1176be646bf9ccfeaa847dc961c
mahe4um1 <138662532+mahe4um1@users.noreply.github.com>
2023-07-05 16:31:16 +0530

    Update SampleHelloWorld.java

--- SampleHelloWorld.java
+++ SampleHelloWorld.java
@@ -3,6 +3,6 @@ package New.java.project;
 public class SampleHelloWorld {
 	
 	public static void main(String[] args){
-			System.out.println("........##########Hello World, welcome to jenkins training ##########........");
+			System.out.println("........##########Made some changes in the first job ##########........");
 		}
 	}
