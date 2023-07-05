317cbb9c36132bd0d3dd719dfe468009e18b16b6
Singam <nagamaheswar.singam@capgemini.com>
2023-07-05 15:56:53 +0530

    Adding new files

---
+++ SampleHelloWorld.java
@@ -0,0 +1,8 @@
+package New.java.project;
+
+public class SampleHelloWorld {
+	
+	public static void main(String[] args){
+			System.out.println("........##########Hello World, welcome to jenkins training ##########........");
+		}
+	}
